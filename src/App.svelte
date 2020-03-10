b36ad650dc916eadd52e7759df2a1a2568b032b1
Alexander Czigler <alexander@iteam.se>
2020-03-10 08:53:11 +0100

    wip: calculator

--- src/App.svelte
+++ src/App.svelte
@@ -8,33 +8,69 @@
 
 	const week = {
 		mon: {
-			tick: 0,
-			tock: 0,
+			am: {
+				tt: 'tick',
+				hours: 0,
+			},
+			pm: {
+				tt: 'tick',
+				hours: 0,
+			},
 		},
 		tue: {
-			tick: 0,
-			tock: 0,
+			am: {
+				tt: 'tick',
+				hours: 0,
+			},
+			pm: {
+				tt: 'tick',
+				hours: 0,
+			},
 		},
 		wed: {
-			tick: 0,
-			tock: 0,
+			am: {
+				tt: 'tick',
+				hours: 0,
+			},
+			pm: {
+				tt: 'tock',
+				hours: 0,
+			},
 		},
 		thu: {
-			tick: 0,
-			tock: 0,
+			am: {
+				tt: 'tock',
+				hours: 0,
+			},
+			pm: {
+				tt: 'tock',
+				hours: 0,
+			},
 		},
 		fri: {
-			tick: 0,
-			tock: 0,
+			am: {
+				tt: 'tock',
+				hours: 0,
+			},
+			pm: {
+				tt: 'tock',
+				hours: 0,
+			},
 		},
+	}
+
+	const summary = {
+		roles: 0,
+		hours: 0,
+		rate: 0,
 	}
 </script>
 
 <main>
-	<h1>Team Estimator</h1>
+	<h1>Estimat</h1>
 
 	<hr />
-	<h2>Roles</h2>
+	<h2>Roller</h2>
 	{#each Object.keys(Roles) as role, i}
 		{role}: <input type="number" bind:value={estimate[role]} />
 		{#if i+1 % 3 === 3}
@@ -43,34 +79,47 @@
 	{/each}
 
 	<hr />
-	<h3>Weekly schedule</h3>
+	<h3>Vecka</h3>
+	<div>
+		<button>Tick</button>
+		<button>Tock</button>
+	</div>
 	<div>
 		<span class="weekday">
 			Måndag<br />
-			<input type="number" bind:value={week.mon.tick} /><br />
-			<input type="number" bind:value={week.mon.tock} /><br />
+			<input type="number" bind:value={week.mon.am.hours} /><br />
+			<input type="number" bind:value={week.mon.pm.hours} /><br />
 		</span>
 		<span class="weekday">
 			Tisdag<br />
-			<input type="number" bind:value={week.tue.tick} /><br />
-			<input type="number" bind:value={week.tue.tock} /><br />
+			<input type="number" bind:value={week.tue.am.hours} /><br />
+			<input type="number" bind:value={week.tue.pm.hours} /><br />
 		</span>
 		<span class="weekday">
 			Onsdag<br />
-			<input type="number" bind:value={week.wed.tick} /><br />
-			<input type="number" bind:value={week.wed.tock} /><br />
+			<input type="number" bind:value={week.wed.am.hours} /><br />
+			<input type="number" bind:value={week.wed.pm.hours} /><br />
 		</span>
 		<span class="weekday">
 			Torsdag<br />
-			<input type="number" bind:value={week.thu.tick} /><br />
-			<input type="number" bind:value={week.thu.tock} /><br />
+			<input type="number" bind:value={week.thu.am.hours} /><br />
+			<input type="number" bind:value={week.thu.pm.hours} /><br />
 		</span>
 		<span class="weekday">
 			Fredag<br />
-			<input type="number" bind:value={week.fri.tick} /><br />
-			<input type="number" bind:value={week.fri.tock} /><br />
+			<input type="number" bind:value={week.fri.am.hours} /><br />
+			<input type="number" bind:value={week.fri.pm.hours} /><br />
 		</span>
 	</div>
+
+	<hr />
+	<h3>Summering</h3>
+	<p>
+		Valt team består av {summary.roles} roller som lägger {summary.hours} timmar i veckan.
+	</p>
+	<p>
+		Förväntad månadskostnad är {summary.rate} kr i veckan.
+	</p>
 </main>
 
 <style>
