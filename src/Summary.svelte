6de36f49e2b15ae9552ee1dc35e90e33bde58697
Alexander Czigler <alexander@iteam.se>
2020-03-12 13:26:09 +0100

    fix: string

--- src/Summary.svelte
+++ src/Summary.svelte
@@ -6,5 +6,5 @@
   Valt team består av <span class="highlight">{summary.roles}</span> roller som tillsammans lägger <span class="highlight">{summary.hours}</span> timmar i veckan.
 </p>
 <p>
-  Förväntad månadskostnad är <span class="highlight">{summary.rate}</span> kr i veckan.
+  Förväntad kostnad är <span class="highlight">{summary.rate}</span> kr i veckan.
 </p>
