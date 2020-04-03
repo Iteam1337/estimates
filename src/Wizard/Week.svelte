aa5ce08fcecc23c743127d134600b5d1407df85d
Alexander Czigler <alexander@iteam.se>
2020-04-03 17:21:11 +0200

    feat: week view styling

--- src/Wizard/Week.svelte
+++ src/Wizard/Week.svelte
@@ -29,7 +29,7 @@
 </script>
 
 <style>
-  div.role {
+  div.week {
     margin: 20px 0;
     display: flex;
     flex-direction: row;
@@ -39,12 +39,7 @@
     align-content: stretch;
   }
 
-  span.count {
-    width: 20%;
-    margin-top: 10px;
-  }
-
-  span.team {
+  span.day {
     width: 70%;
     display: flex;
     flex-direction: column;
@@ -52,29 +47,55 @@
     justify-content: flex-start;
     align-items: stretch;
     align-content: stretch;
+    text-align: left;
   }
 
-  label {
-    font-size: 20px;
-    font-weight: 800;
+  .round {
+    width: 20%;
+    position: relative;
+    margin: 8px 0 0 15px;
+  }
+
+  .round label {
+    background-color: #ffffff;
+    border: 2px solid #aaaaaa;
+    border-radius: 50%;
+    cursor: pointer;
+    height: 50px;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 50px;
+  }
+
+  .round input[type='checkbox'] {
+    visibility: hidden;
+  }
+
+  .round input[type='checkbox']:checked + label {
+    background-color: #66bb6a;
+    border-color: #66bb6a;
+  }
+
+  .round input[type='checkbox']:checked + label:after {
+    opacity: 1;
   }
 </style>
 
-<div class="week">
-  <h1>När ska vi jobba ihop?</h1>
+<h1>När ska vi jobba ihop?</h1>
 
-  {#each Object.keys(state.week) as day, index}
-    <div class="role">
-      <span class="count">
-        <input
-          type="checkbox"
-          bind:checked={state.week[day]}
-          on:change={weekUpdated}
-          id={day} />
-      </span>
-      <span class="team">
-        <label for={day}>{weekLabels[day]}</label>
-      </span>
-    </div>
-  {/each}
-</div>
+{#each Object.keys(state.week) as day, index}
+  <div class="week">
+    <span class="count round">
+      <input
+        type="checkbox"
+        bind:checked={state.week[day]}
+        on:change={weekUpdated}
+        id={day} />
+      <label for={day} />
+    </span>
+    <span class="day">
+      <h2>{weekLabels[day]}</h2>
+    </span>
+  </div>
+{/each}
