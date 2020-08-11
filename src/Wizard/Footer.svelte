abf2a01d62af1dd937bacdf62a3bc538ef4fb82e
Alexander Czigler <alexander@iteam.se>
2020-08-11 16:41:42 +0200

    feat: footer styling

--- src/Wizard/Footer.svelte
+++ src/Wizard/Footer.svelte
@@ -1,35 +1,59 @@
 <style>
-  div {
+  div.footer {
     position: absolute;
     bottom: 0;
     width: 100%;
     border-top: 1px black solid;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: flex-start;
+    align-items: flex-end;
+    align-content: stretch;
   }
 
-  span {
-    border-left: 1px black solid;
+  div.area-link {
+    height: 4em;
+    border-right: 1px black solid;
+    text-align: center;
   }
 
-  span:first-child {
-    border-left: 0;
+  @media (max-width: 640px) {
+    div.area-link:last-child {
+      border-right: 0;
+    }
+
+    div.area-link {
+      width: 25%;
+    }
   }
 
-  span > a {
-    margin: 2em 1em;
+  @media (min-width: 640px) {
+    div.area-link > p {
+      margin: 2em;
+    }
   }
 </style>
 
-<div>
-  <span>
-    <a href="/">Översikt</a>
-  </span>
-  <span>
-    <a href="/team">Team</a>
-  </span>
-  <span>
-    <a href="/week">Vecka</a>
-  </span>
-  <span>
-    <a href="/plan">Planering</a>
-  </span>
+<div class="footer">
+  <div class="area-link">
+    <p>
+      <a href="/">Översikt</a>
+    </p>
+  </div>
+  <div class="area-link">
+    <p>
+      <a href="/team">Team</a>
+    </p>
+  </div>
+  <div class="area-link">
+    <p>
+      <a href="/week">Vecka</a>
+    </p>
+  </div>
+  <div class="area-link">
+    <p>
+      <a href="/plan">Planering</a>
+    </p>
+  </div>
 </div>
